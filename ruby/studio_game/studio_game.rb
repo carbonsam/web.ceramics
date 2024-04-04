5b350e2ad21bde935581abf5926d5357cb1db177
Samuel Schumacher <samuel@schumacher.family>
2024-04-04 12:58:44 -0500

    Ruby Game - Lesson 4

--- ruby/studio_game/studio_game.rb
+++ ruby/studio_game/studio_game.rb
@@ -1,7 +1,7 @@
 puts "Let's play a game!\n\t3\n\t2\n\t1\n"
 puts "🚀" * 25
 
-player = "Finn"
+name = "  \n finn \t "
 health = 60
 
-puts "#{player}'s health is #{health}"
+puts "#{name.strip.capitalize.ljust(30, ".")} #{health} health"
