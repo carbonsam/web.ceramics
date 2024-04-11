cd04f7866bccb4a33a74ce6fae4a5342f71a08e9
Samuel Schumacher <samuel@schumacher.family>
2024-04-11 12:55:45 -0500

    Ruby Block Course: Lesson 3

--- ruby/blocks/block_basics.rb
+++ ruby/blocks/block_basics.rb
@@ -6,4 +6,11 @@ flyers = []
   flyers << Flyer.new("Flyer #{x}", "flyer#{x}@example.com", x * 1000)
 end
 
-puts flyers
+flyers.each { |f| puts "#{f.name} - #{f.miles_flown} miles" }
+
+sum = 0
+flyers.each { |f| sum += f.miles_flown}
+puts "Total miles flown: #{sum}"
+
+promotions = { "United" => 1.5, "Delta" => 2.0, "Lufthansa" => 2.5 }
+promotions.each { |k, v| puts "Earn #{v}x miles by flying #{k}!" }
