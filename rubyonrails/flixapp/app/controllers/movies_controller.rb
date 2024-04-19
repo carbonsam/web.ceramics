998c601a7a3e1fe702d34212687d84a017a48b5a
Samuel Schumacher <samuel@schumacher.family>
2024-04-19 15:30:07 -0500

    Rails Tutorial: Lessons 13-19

--- rubyonrails/flixapp/app/controllers/movies_controller.rb
+++ rubyonrails/flixapp/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
@@ -14,8 +14,38 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_attributes = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
-    @movie.update(movie_attributes)
-    redirect_to @movie
+
+    if @movie.update(movie_attributes)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_attributes)
+
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+private
+
+  def movie_attributes
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration, :image_file_name)
+  end
+
 end
