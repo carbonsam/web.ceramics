37d4b3101f67db4659c6370ec72aec54ef7f5a69
Samuel Schumacher <samuel@schumacher.family>
2024-05-20 17:13:57 -0500

    Rails Tutorial: Lesson 22

--- rubyonrails/flixapp/app/models/review.rb
+++ rubyonrails/flixapp/app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :comment, length: { minimum: 4 }
+  validates :stars, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
 end
