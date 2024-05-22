4da1f44a0c9106dfb5afd6470b1aa0a02b7e9bea
Samuel Schumacher <samuel@schumacher.family>
2024-05-22 17:17:11 -0500

    Rails Tutorial: Lesson 28

--- rubyonrails/flixapp/app/controllers/users_controller.rb
+++ rubyonrails/flixapp/app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -21,6 +25,26 @@ class UsersController < ApplicationController
       redirect_to user_path(@user), notice: 'User was successfully created.'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      redirect_to root_path, status: :see_other, alert: 'User was successfully destroyed.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
