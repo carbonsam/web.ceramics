4ca7815b27cfd4a11314c8a70c38b69eafb3ee6e
Samuel Schumacher <samuel@schumacher.family>
2024-05-28 15:52:21 -0500

    Rails Tutorial: Lesson 30

--- rubyonrails/flixapp/app/controllers/sessions_controller.rb
+++ rubyonrails/flixapp/app/controllers/sessions_controller.rb
@@ -7,7 +7,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
