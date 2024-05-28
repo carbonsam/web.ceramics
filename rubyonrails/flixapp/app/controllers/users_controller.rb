22ecc3c744395828a1384c13497d00f9de774925
Samuel Schumacher <samuel@schumacher.family>
2024-05-28 16:08:01 -0500

    Rails Tutorial: Lesson 31

--- rubyonrails/flixapp/app/controllers/users_controller.rb
+++ rubyonrails/flixapp/app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: 'User was successfully created.'
     else
       render :new, status: :unprocessable_entity
