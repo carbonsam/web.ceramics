2ca1de49644c6829b7c5a9e8f401258ab15cf81b
Samuel Schumacher <samuel@schumacher.family>
2024-05-29 16:20:04 -0500

    Rails Tutorial: Lesson 38

--- rubyonrails/flixapp/app/controllers/movies_controller.rb
+++ rubyonrails/flixapp/app/controllers/movies_controller.rb
@@ -12,6 +12,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
     @favorite = current_user.favorites.find_by(movie_id: @movie.id) if current_user
+    @genres = @movie.genres
   end
 
   def edit
@@ -52,6 +53,6 @@ class MoviesController < ApplicationController
 
   def movie_attributes
     params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration,
-                                  :image_file_name)
+                                  :image_file_name, genre_ids: [])
   end
 end
---
+++ rubyonrails/flixapp/app/models/genre.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Genre < ApplicationRecord
+  has_many :characterizations, dependent: :destroy
+  has_many :movies, through: :characterizations
+
+  validates :name, presence: true, uniqueness: true
+
+end
