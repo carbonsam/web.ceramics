312a9dab88837a0f873f8e806b8e53d0391158ce
Samuel Schumacher <samuel@schumacher.family>
2024-05-29 11:36:23 -0500

    Rails Tutorial: Lesson 34

--- rubyonrails/flixapp/app/controllers/application_controller.rb
+++ rubyonrails/flixapp/app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 
 class ApplicationController < ActionController::Base
 
+  private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -12,11 +14,22 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user?
 
+  def current_user_admin?
+    current_user&.admin?
+  end
+  helper_method :current_user_admin?
+
   def require_signin
     return if current_user
 
     session[:intended_url] = request.url
     redirect_to(signin_path, alert: 'You need to sign in first!')
+  end
+
+  def require_admin
+    return if current_user_admin?
+
+    redirect_to(root_path, alert: 'You must be an admin to do that!')
   end
 
 end
--- rubyonrails/flixapp/app/controllers/movies_controller.rb
+++ rubyonrails/flixapp/app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
+
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @movies = Movie.released
   end
