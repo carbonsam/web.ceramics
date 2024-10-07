b046170ee6d6665286bd691c8ac080c29a1bb7f2
Samuel Schumacher <samuel@schumacher.family>
2024-10-07 13:27:00 -0500

    Remove fishing project

--- rubyonrails/fishing/app/controllers/activity_controller.rb
+++
@@ -1,9 +0,0 @@
-class ActivityController < ApplicationController
-  before_action :require_signin
-
-  def index
-    @fish_catches = FishCatch.order(created_at: :desc).includes(:user, :bait)
-
-    @fish_catches = current_user.assign_my_likes_to_catches(@fish_catches)
-  end
-end
--- rubyonrails/fishing/app/models/like.rb
+++
@@ -1,4 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :fish_catch, counter_cache: true
-end
